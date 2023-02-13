a1b6890c703e02ecd9e8a10befa7540915b3c6a0
pi <zacrify1986@gmail.com>
2023-02-13 12:59:13 +0000

    with voice or voiceless

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var allAudios = document.querySelectorAll('audio');
+// let voice = document.getElementsByClassName('voice');
+// let voiceless = document.getElementsByClassName('voiceless');
+let voice = document.getElementById('voice-sound');
+// voice1.innerHTML = '<h1>Voice</h1>';
 
 function stopAllAudio(){
 	allAudios.forEach(function(audio){
@@ -10,4 +14,11 @@ function play(element){
     stopAllAudio();
     var audio = document.getElementById(element);
     audio.play();
+    let newArray = element.split('-');
+    let byValue = document.querySelectorAll(`[value="${newArray[0]}"]`);
+    if(byValue[0].classList.contains('voice')) {
+        voice.innerHTML = '<h1>Voice</h1>'
+    } else {
+        voice.innerHTML = '<h1>VoiceLESS</h1>'
+    }
 }
